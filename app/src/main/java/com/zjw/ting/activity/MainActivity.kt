63436c1897c670c7d132485d9b11abe09fce8e9d
zjw <819151780@qq.com>
2019-06-01 01:01:55 +0800

    尝试更新

--- app/src/main/java/com/zjw/ting/activity/MainActivity.kt
+++ app/src/main/java/com/zjw/ting/activity/MainActivity.kt
@@ -2,11 +2,15 @@ package com.zjw.ting.activity
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.eye.cool.permission.PermissionHelper
@@ -17,32 +21,36 @@ import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_main.*
 import top.defaults.drawabletoolbox.DrawableBuilder
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_APP_INSTALL: Int = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var permissions = arrayListOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            //Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_NETWORK_STATE,
-            Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.READ_LOGS
-            )
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            permissions.add(Manifest.permission.REQUEST_INSTALL_PACKAGES)
-        }
-
         PermissionHelper.Builder(this@MainActivity)
             .permissions(
-                permissions.toTypedArray()
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.ACCESS_WIFI_STATE
+                )
+                //permissions.toTypedArray()
             ).permissionCallback {
                 if (it) {
                     // 请求权限成功
+                    //Toasty.success(this@MainActivity, "权限申请成功~", Toast.LENGTH_SHORT, true).show()
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        var hasInstallPermission = isHasInstallPermissionWithO(this@MainActivity)
+                        if (!hasInstallPermission) {
+                            Toasty.success(this@MainActivity, "需要应用安装权限，前往设置~~", Toast.LENGTH_SHORT, true).show()
+                            startInstallPermissionSettingActivity(this@MainActivity)
+                        }
+                    }
                 } else {
                     // 请求权限失败
                     Toasty.warning(this@MainActivity, "亲，缺少必要的权限，打扰了~", Toast.LENGTH_SHORT, true).show()
@@ -50,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }.build()
             .request()
+
 
         val skills = HistoryCache.toArray(applicationContext)
         val skillHots = arrayListOf<String>("我当算命先生那几年", "官方救世主", "我有一座恐怖屋", "剑来")
@@ -95,5 +104,25 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, HistoryActivity::class.java)
             startActivity(intent)
         }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun isHasInstallPermissionWithO(context: Context): Boolean {
+        return context.packageManager.canRequestPackageInstalls()
+    }
+
+
+    /**
+     * 开启设置安装未知来源应用权限界面
+     * @param context
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun startInstallPermissionSettingActivity(context: Context?) {
+        if (context == null) {
+            return
+        }
+        val intent = Intent(ACTION_MANAGE_UNKNOWN_APP_SOURCES)
+        (context as Activity).startActivityForResult(intent, REQUEST_CODE_APP_INSTALL)
     }
 }
